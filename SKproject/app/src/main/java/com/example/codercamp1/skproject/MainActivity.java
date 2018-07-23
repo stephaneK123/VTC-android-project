7bac3014c76a873a56da26d87df62350368bee1a
Stephanekkk <Swar.kung@gmail.com>
2018-07-23 15:50:19 -0400

    init

---
+++ SKproject/app/src/main/java/com/example/codercamp1/skproject/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.codercamp1.skproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
